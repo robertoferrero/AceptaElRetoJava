2f1fd6c7f08fa2a39a30844168b057b22a566135
Roberto Ferrero <robertoferrero.org@gmail.com>
2021-03-27 16:49:15 +0100

    primer commit

---
+++ A167_PintandoFractales.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author rferrero
+ */
+public class A167_PintandoFractales {
+ 
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+                
+        while(entrada.hasNext()) {
+            String s = entrada.nextLine();
+            int costat = Integer.parseInt(s);
+            System.out.println(calculaPintura(costat));                        
+        }                
+    }
+    
+    static int calculaPintura(int costat) {
+        if(costat==1)
+            return 4;
+        return 4*costat+4*calculaPintura(costat/2);
+        
+    }
+}
