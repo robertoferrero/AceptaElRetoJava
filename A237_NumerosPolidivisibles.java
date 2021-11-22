8a020178129047e8f5130130fb4d7eacad65825c
robertoferrero <robertoferrero.org@gmail.com>
2021-11-22 20:29:42 +0100

    Añadidas alternativas al cálculo del número de dígitos

--- A237_NumerosPolidivisibles.java
+++ A237_NumerosPolidivisibles.java
@@ -1,22 +1,27 @@
+package aceptaelreto;
 
 import java.util.Scanner;
 
-/**
- *
- * @author rferrero
- */
 public class A237_NumerosPolidivisibles {
- 
-    public static void main(String[] args) {
+
+	public static void main(String[] args) {
         
         Scanner entrada = new Scanner(System.in);
         
         while (entrada.hasNextLong())
         {
-            //long numero = 381654729;
             long numero = entrada.nextLong();
-            int longitud = String.valueOf(numero).length();
+            //int longitud = String.valueOf(numero).length();
+            //int longitud = (int)Math.log10(numero) + 1;
+            int longitud = 0;
+            long aux = numero;
+            while (aux > 0)
+            {
+            	++longitud;
+            	aux /= 10;
+            }
 
+            
             boolean polidivisible = true;
             while (longitud > 1 && polidivisible)
             {
@@ -27,9 +32,8 @@ public class A237_NumerosPolidivisibles {
                 }
                 else                
                     polidivisible = false;
-
             }
-            System.out.println(polidivisible?"POLIDIVISIBLE":"NO POLIDIVISIBLE");
+            System.out.println(polidivisible ? "POLIDIVISIBLE" : "NO POLIDIVISIBLE");
         }
         
     }
